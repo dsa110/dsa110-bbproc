5e6d1fc34a3e42d2268bd375731d047bc6a6b702
greghell <greg.hellbourg@gmail.com>
2021-01-14 17:45:28 -0800

    Update dsacorr.c

--- dsacorr.c
+++ dsacorr.c
@@ -1,58 +1,161 @@
 /*
-gcc -o dsacorr corr.c -I/usr/local/include -L/usr/local/lib -lm -g -O2 -L/usr/lib/gcc/x86_64-linux-gnu/5 -lgfortran
-
+gcc -o beamformer beamformer.c -I/usr/local/include -L/usr/local/lib -lm -g -O2 -L/usr/lib/gcc/x86_64-linux-gnu/5 -lgfortran
+python beamformer was too slow, decided to use python to write up header etc but do actual beamforming in C.
+This code should take 5 parameters:
 * data file name
+* calibration file name
+* start frequency
+* separation
+* beam number
 * output file name
-
+assumes 48 channels for beamformer (weights for 8 data channels), ONLY 1 beam
+greg hellbourg
+ghellbourg@astro.caltech.edu
 */
 
 
-#include "stdio.h"
-#include "stdlib.h"
-#include "sys/types.h"
-#include "sys/socket.h"
-#include "string.h"
-#include "netinet/in.h"
+#include "stdio.h"  
+#include "stdlib.h"  
+#include "sys/types.h"  
+#include "sys/socket.h"  
+#include "string.h"  
+#include "netinet/in.h"  
 #include "netdb.h"
 #include <unistd.h>
 #include <pthread.h>
 #include <arpa/inet.h>
 #include <math.h>
 
+int NW = 48;	// number of channels for the beamformer
+float PI = 3.141592653589793238;
+float CVAC = 299792458;
+
+
+
+int init_weights(char * fnam, float *antpos, float *weights, int nPols) {
+
+	// assumes 64 antennas
+	// antpos: takes only easting
+	// weights: takes [ant, NW==48] 
+
+	FILE *fin;
+	FILE *fants;
+	int rd;
+
+	fin=fopen(fnam,"rb");
+
+	rd = fread(antpos,64*sizeof(float),1,fin);
+	rd = fread(weights,64*NW*nPols*2*sizeof(float),1,fin);
+	float wnorm;
+	for (int i=0;i<64*NW*nPols;i++) {
+		wnorm = sqrt(weights[2*i]*weights[2*i] + weights[2*i+1]*weights[2*i+1]);
+		if (wnorm!=0.0) {
+			weights[2*i] /= wnorm;
+			weights[2*i+1] /= wnorm;
+		}
+	}
+
+	fclose(fin);
+	return 0;
+
+}
+
+void calc_weights(float *antpos, float *weights, float *freqs, float *wr, float *wi, float sep, int nBeamNum, int nPols) {
+
+
+	float theta, afac, twr, twi;
+	
+	theta = sep*(127.-(float)nBeamNum)*PI/10800.; // radians
+	for(int nAnt=0;nAnt<64;nAnt++){
+		for(int nChan=0;nChan<48;nChan++){
+			for(int nPol=0;nPol<nPols;nPol++){
+				afac = -2.*PI*freqs[nChan*8+4]*theta/CVAC; // factor for rotate
+				twr = cos(afac*antpos[nAnt]);
+				twi = sin(afac*antpos[nAnt]);
+
+				wr[nAnt*(48*nPols)+nChan*nPols+nPol] = (twr*weights[(nAnt*(48*nPols)+nChan*nPols+nPol)*2] - twi*weights[(nAnt*(48*nPols)+nChan*nPols+nPol)*2+1]);
+				wi[nAnt*(48*nPols)+nChan*nPols+nPol] = (twi*weights[(nAnt*(48*nPols)+nChan*nPols+nPol)*2] + twr*weights[(nAnt*(48*nPols)+nChan*nPols+nPol)*2+1]);
+			}
+		}
+	}
+
+
+}
+
+void beamformer(char *input, float *wr, float *wi, unsigned char *output, int nChans, int nAnts, int nTimes, int nPols) {
+
+	float inr_x, ini_x, inr_y, ini_y;
+	float wrx, wix, wry, wiy;
+	float rx, ix, ry, iy;
+	float tmp;
+	
+	for(int nTime=0;nTime<nTimes;nTime++){
+		for(int nChan=0;nChan<48;nChan++){
+			for(int i=0;i<8;i++){
+				tmp = 0;
+				for(int nAnt=0;nAnt<nAnts;nAnt++){
+					inr_x = (float)(((char)((input[nAnt*(nChans*nTimes*nPols)+(nChan*8+i)*(nTimes*nPols)+nTime*nPols] & 15) << 4)) >> 4);
+					ini_x = (float)(((char)((input[nAnt*(nChans*nTimes*nPols)+(nChan*8+i)*(nTimes*nPols)+nTime*nPols] & 240))) >> 4);
+					inr_y = (float)(((char)((input[nAnt*(nChans*nTimes*nPols)+(nChan*8+i)*(nTimes*nPols)+nTime*nPols+1] & 15) << 4)) >> 4);
+					ini_y = (float)(((char)((input[nAnt*(nChans*nTimes*nPols)+(nChan*8+i)*(nTimes*nPols)+nTime*nPols+1] & 240))) >> 4);
+					
+					wrx = wr[nAnt*(48*nPols)+nChan*nPols];
+					wix = wi[nAnt*(48*nPols)+nChan*nPols];
+					wry = wr[nAnt*(48*nPols)+nChan*nPols+1];
+					wiy = wi[nAnt*(48*nPols)+nChan*nPols+1];
+					
+					rx = inr_x*wrx - ini_x*wix;
+					ix = inr_x*wix + ini_x*wrx;
+					ry = inr_y*wry - ini_y*wiy;
+					iy = inr_y*wiy + ini_y*wry;
+					
+					tmp += (rx*rx+ix*ix+ry*ry+iy*iy) / nAnts;
+				}
+				output[nTime*nChans+nChan*8+i] = (unsigned char)tmp;
+			}
+		}
+	}
+	
+	
+}
+
 void usage()
 {
   fprintf (stdout,
-	   "correlator [options]\n"
+	   "t3_beamformer [options]\n"
 	   " -d voltage data file name [no default]\n"
+	   " -f calibration file name[no default]\n"
 	   " -o output file name[no default]\n"
-	   " -t time integration in ms [default : 0.03]\n"
-	   " -f frequency resolution in MHz [default : 0.03]\n"
+	   " -z fch1 in MHz [default 1530]\n"
+	   " -s interbeam separation in arcmin [default 1.4]\n"
+	   " -n beam number [0 -- 255, default 127]\n"
 	   " -h print usage\n");
 }
 
 
 int main (int argc, char *argv[]) {
 	
-	// data file constants
-	int nAnts = 24;
-	int nChans = 384;
-	int nTimes = 2;
-	int nPols = 2;
-	float fDF = 0.03051757812; // frequency bandwidth per channel in MHz
 	
-	// read params : fInt, fRes
+	// read params : fch1, fnam, fdataname, sep
 	int arg = 0;
-	float fInt = 1./fDF/1000.;	// time integration in ms
-	float fRes = fDF;	// frequency resolution in MHz
-	
+	float fch1 = 1530.0;
+	float sep = 1.4;
+	int nBeamNum = 127;
+	int nChans = 384;
+	int nAnts = 24;
+	int nPols = 2;
+	int nTimes = 2;
+	char * fnam;
+	fnam=(char *)malloc(sizeof(char)*200);
+	sprintf(fnam,"nofile");
 	char * fdata;
-	fdata=(char *)malloc(sizeof(char)*100);
+	fdata=(char *)malloc(sizeof(char)*200);
 	sprintf(fdata,"nofile");
 	char * fout;
-	fout=(char *)malloc(sizeof(char)*100);
+	fout=(char *)malloc(sizeof(char)*200);
 	sprintf(fout,"nofile");
 	
-	while ((arg=getopt(argc,argv,"d:o:t:f:h")) != -1)
+	while ((arg=getopt(argc,argv,"d:f:o:z:s:n:h")) != -1)
 	{
 		switch (arg)
 		{
@@ -68,6 +171,18 @@ int main (int argc, char *argv[]) {
 				usage();
 				return EXIT_FAILURE;
 			}
+			case 'f':
+			if (optarg)
+			{
+				strcpy(fnam,optarg);
+				break;
+			}
+			else
+			{
+				printf("-f flag requires argument");
+				usage();
+				return EXIT_FAILURE;
+			}
 			case 'o':
 			if (optarg)
 			{
@@ -80,29 +195,66 @@ int main (int argc, char *argv[]) {
 				usage();
 				return EXIT_FAILURE;
 			}
-			case 't':
+			case 'z':
 			if (optarg)
 			{
-				fInt = atof(optarg);
+				fch1 = atof(optarg);
 				break;
 			}
-			case 'f':
+			else
+			{
+				printf("-z flag requires argument");
+				usage();
+				return EXIT_FAILURE;
+			}
+			case 's':
 			if (optarg)
 			{
-				fRes = atof(optarg);
+				sep = atof(optarg);
 				break;
 			}
+			else
+			{
+				printf("-s flag requires argument");
+				usage();
+				return EXIT_FAILURE;
+			}		
+			case 'n':
+			if (optarg)
+			{
+				nBeamNum = atoi(optarg);
+				break;
+			}
+			else
+			{
+				printf("-n flag requires argument");
+				usage();
+				return EXIT_FAILURE;
+			}				
 			case 'h':
 			usage();
 			return EXIT_SUCCESS;
 		}
 	}
 	
+	
+	// compute beamformer weights
+	unsigned char * output = (char *)malloc(sizeof(char)*nChans*nTimes);
+	unsigned char * input = (char *)malloc(sizeof(char)*nAnts*nChans*nTimes*nPols);
+	float * antpos = (float *)malloc(sizeof(float)*64); // easting
+	float * weights = (float *)malloc(sizeof(float)*64*NW*nPols*2); // complex weights [ant, NW, pol, r/i]
+	float * wr = (float *)malloc(sizeof(float)*64*NW*nPols); // complex weights [ant, NW, pol]
+	float * wi = (float *)malloc(sizeof(float)*64*NW*nPols); // complex weights [ant, NW, pol]
+	float * freqs = (float *)malloc(sizeof(float)*nChans); // freq
+	for (int i=0;i<nChans;i++) freqs[i] = (fch1 - i*250./8192.)*1e6;
+	init_weights(fnam,antpos,weights,nPols);
+	calc_weights(antpos,weights,freqs,wr,wi,sep,nBeamNum,nPols);
+ 
 	FILE *ptr;
 	FILE *write_ptr;
-	ptr = fopen(fdata,"rb");
-	write_ptr = fopen(fout,"wb");
-	int rd;
+	
+	ptr = fopen(fdata,"rb");  // r for read, b for binary
+	write_ptr = fopen(fout,"wb");  // w for write, b for binary
 	
 	long int sz;
 	fseek(ptr, 0L, SEEK_END);
@@ -110,111 +262,16 @@ int main (int argc, char *argv[]) {
 	rewind(ptr);
 	int nTotSam = (int)(floor(sz / (nAnts*nChans*nTimes*nPols)));
 	
-	char * corrs = (char *)malloc(sizeof(char)*(int)(nChans*nAnts*(nAnts-1)/2*nPols*2));
-	unsigned char * autos = (unsigned char *)malloc(sizeof(char)*(int)(nChans*nAnts*nPols));
-	
-	
-	unsigned char * input = (unsigned char *)malloc(sizeof(char)*nAnts*nChans*nTimes*nPols);
-	
-	/* autos : [time, chan, antenna, pol] */
-	/* corr_real : [time, chan, antenna1, antenna2, pol] */
-	
-	int nTimeSam = (int)ceil((double)(fInt / (2./fDF/1000.)));	// number of time integration
-	if (nTimeSam > nTotSam) nTimeSam = nTotSam;
-	if (nTimeSam <= 0) nTimeSam = 1;
-	int nFreqsSam = (int)floor((double)(fRes / fDF));	// number of frequency integration
-	if (nFreqsSam > nChans) nFreqsSam = nChans;
-	if (nFreqsSam <= 0) nFreqsSam = 1;
-	
-	// find averages over freq channels so that all channels are used
-	int idx = 0;
-	while(nChans%(nFreqsSam+idx) != 0) idx++;
-	nFreqsSam = nFreqsSam+idx;
-	
-	// arrays to be written
-	char * corrswrite = (char *)malloc(sizeof(char)*(int)(nChans/nFreqsSam*nAnts*(nAnts-1)/2*nPols*2));
-	unsigned char * autoswrite = (unsigned char *)malloc(sizeof(char)*(int)(nChans/nFreqsSam*nAnts*nPols));
-	
-	float in1rx, in1ix, in1ry, in1iy;
-	float in2rx, in2ix, in2ry, in2iy;
-	int i, j, nSam, nt, nfr, npl, nant1, nant2, timeslo;
-	
-	printf("\n");
-	printf("processing file %s.\n", fdata);
-	printf("found %d time samples.\n", nTotSam);
-	printf("writing to file %s.\n", fout);
-	printf("time resolution : %f ms.\n", nTimeSam * 2./fDF/1000.);
-	printf("averaging %d time samples together.\n", (int)(nTimeSam*2));
-	printf("frequency resolution : %f MHz.\n", nFreqsSam * fDF);
-	printf("averaging %d frequency channels together.\n", nFreqsSam);
-	printf("\n");
-	
-	
-	for(timeslo = 0; timeslo < (int)(nTotSam/nTimeSam); timeslo++) {
+	int rd;
+	for(int nSam = 0; nSam < nTotSam; nSam++) {
 		
-//		printf("writing time sample %d / %d\n", timeslo+1, (int)(nTotSam/nTimeSam));
+		rd = fread(input,nAnts*nChans*nTimes*nPols,1,ptr);
+
+		beamformer(input,wr,wi,output,nChans,nAnts,nTimes,nPols);
+
+		fwrite(output,nChans*nTimes,1,write_ptr);
 		
-		memset(autos,0,(int)(nChans*nAnts*nPols));
-		memset(corrs,0,(int)(nChans*nAnts*(nAnts-1)/2*nPols*2));
-		for(nSam = 0; nSam < nTimeSam; nSam++) {	// avg sets of 2 time samples
-			rd = fread(input,nAnts*nChans*nTimes*nPols,1,ptr);
-			for(nt = 0; nt < nTimes; nt++) {
-				for(nfr = 0; nfr < nChans; nfr++) {
-					for(npl = 0; npl < nPols; npl++) {
-						for(nant1 = 0; nant1 < nAnts; nant1++) {
-							
-							in1rx = (float)(((char)((input[nant1*(nChans*nTimes*nPols)+nfr*(nTimes*nPols)+nt*nPols] & 15) << 4)) >> 4);
-							in1ix = (float)(((char)((input[nant1*(nChans*nTimes*nPols)+nfr*(nTimes*nPols)+nt*nPols] & 240))) >> 4);
-							in1ry = (float)(((char)((input[nant1*(nChans*nTimes*nPols)+nfr*(nTimes*nPols)+nt*nPols+1] & 15) << 4)) >> 4);
-							in1iy = (float)(((char)((input[nant1*(nChans*nTimes*nPols)+nfr*(nTimes*nPols)+nt*nPols+1] & 240))) >> 4);
-							autos[nfr*(nAnts*nPols)+nant1*nPols] += (char)(in1rx*in1rx + in1ix*in1ix);
-							autos[nfr*(nAnts*nPols)+nant1*nPols+1] += (char)(in1ry*in1ry + in1iy*in1iy);
-							
-							for(nant2 = nant1+1; nant2 < nAnts; nant2++) {
-								in2rx = (float)(((char)((input[nant2*(nChans*nTimes*nPols)+nfr*(nTimes*nPols)+nt*nPols] & 15) << 4)) >> 4);
-								in2ix = (float)(((char)((input[nant2*(nChans*nTimes*nPols)+nfr*(nTimes*nPols)+nt*nPols] & 240))) >> 4);
-								in2ry = (float)(((char)((input[nant2*(nChans*nTimes*nPols)+nfr*(nTimes*nPols)+nt*nPols+1] & 15) << 4)) >> 4);
-								in2iy = (float)(((char)((input[nant2*(nChans*nTimes*nPols)+nfr*(nTimes*nPols)+nt*nPols+1] & 240))) >> 4);
-								
-								corrs[ nfr*(nAnts*(nAnts-1)/2*nPols*2) + ((nAnts-1)*nAnts/2 - (nAnts-1-nant1)*(nAnts-nant1)/2 + nant2 - (nant1+1))*nPols*2    ] += (char)(in1rx*in2rx + in1ix*in2ix);
-								corrs[ nfr*(nAnts*(nAnts-1)/2*nPols*2) + ((nAnts-1)*nAnts/2 - (nAnts-1-nant1)*(nAnts-nant1)/2 + nant2 - (nant1+1))*nPols*2 + 1] += (char)(-in1rx*in2ix + in1ix*in2rx);
-								corrs[ nfr*(nAnts*(nAnts-1)/2*nPols*2) + ((nAnts-1)*nAnts/2 - (nAnts-1-nant1)*(nAnts-nant1)/2 + nant2 - (nant1+1))*nPols*2 + 2] += (char)(in1ry*in2ry + in1iy*in2iy);
-								corrs[ nfr*(nAnts*(nAnts-1)/2*nPols*2) + ((nAnts-1)*nAnts/2 - (nAnts-1-nant1)*(nAnts-nant1)/2 + nant2 - (nant1+1))*nPols*2 + 3] += (char)(-in1ry*in2iy + in1iy*in2ry);
-								
-								
-							}
-						}
-					}
-				}
-			}
-		}
-		// average over frequencies
-		memset(autoswrite,0,(int)(nChans/nFreqsSam*nAnts*nPols));
-		memset(corrswrite,0,(int)(nChans/nFreqsSam*nAnts*(nAnts-1)/2*nPols*2));
-		for (i = 0; i < (int)(nChans/nFreqsSam); i++){
-			for (j = 0; j < nFreqsSam; j++){
-				for (nant1 = 0; nant1 < nAnts; nant1++) {
-					autoswrite[i*nAnts*nPols+nant1*nPols] += autos[(i*nFreqsSam+j)*nAnts*nPols+nant1*nPols];
-					autoswrite[i*nAnts*nPols+nant1*nPols+1] += autos[(i*nFreqsSam+j)*nAnts*nPols+nant1*nPols+1];
-					for (nant2 = nant1+1; nant2 < nAnts; nant2++) {
-						corrswrite[i*(nAnts*(nAnts-1)/2*nPols*2) + ((nAnts-1)*nAnts/2 - (nAnts-1-nant1)*(nAnts-nant1)/2 + nant2 - (nant1+1))*nPols*2    ] += corrs[ (i*nFreqsSam+j)*(nAnts*(nAnts-1)/2*nPols*2) + ((nAnts-1)*nAnts/2 - (nAnts-1-nant1)*(nAnts-nant1)/2 + nant2 - (nant1+1))*nPols*2    ];
-						corrswrite[i*(nAnts*(nAnts-1)/2*nPols*2) + ((nAnts-1)*nAnts/2 - (nAnts-1-nant1)*(nAnts-nant1)/2 + nant2 - (nant1+1))*nPols*2 + 1] += corrs[ (i*nFreqsSam+j)*(nAnts*(nAnts-1)/2*nPols*2) + ((nAnts-1)*nAnts/2 - (nAnts-1-nant1)*(nAnts-nant1)/2 + nant2 - (nant1+1))*nPols*2 + 1];
-						corrswrite[i*(nAnts*(nAnts-1)/2*nPols*2) + ((nAnts-1)*nAnts/2 - (nAnts-1-nant1)*(nAnts-nant1)/2 + nant2 - (nant1+1))*nPols*2 + 2] += corrs[ (i*nFreqsSam+j)*(nAnts*(nAnts-1)/2*nPols*2) + ((nAnts-1)*nAnts/2 - (nAnts-1-nant1)*(nAnts-nant1)/2 + nant2 - (nant1+1))*nPols*2 + 2];
-						corrswrite[i*(nAnts*(nAnts-1)/2*nPols*2) + ((nAnts-1)*nAnts/2 - (nAnts-1-nant1)*(nAnts-nant1)/2 + nant2 - (nant1+1))*nPols*2 + 3] += corrs[ (i*nFreqsSam+j)*(nAnts*(nAnts-1)/2*nPols*2) + ((nAnts-1)*nAnts/2 - (nAnts-1-nant1)*(nAnts-nant1)/2 + nant2 - (nant1+1))*nPols*2 + 3];
-					}
-				}
-			}
-		}
-		
-		for (i = 0; i < nChans/nFreqsSam*nAnts*nPols; i++) autoswrite[i] = (char)(autoswrite[i] / nTimes / nTimeSam / nFreqsSam);
-		for (i = 0; i < nChans/nFreqsSam*nAnts*(nAnts-1)/2*nPols*2; i++) corrswrite[i] = (char)(corrswrite[i] / nTimes / nTimeSam / nFreqsSam);
-		for (i = 0; i < nChans/nFreqsSam; i++) {
-			fwrite(&autoswrite[i*nAnts*nPols],nAnts*nPols,1,write_ptr);
-			fwrite(&corrswrite[i*nAnts*(nAnts-1)/2*nPols*2],nAnts*(nAnts-1)/2*nPols*2,1,write_ptr);
-		}
 	}
-
-
 	fclose(ptr);
 	fclose(write_ptr);
 
